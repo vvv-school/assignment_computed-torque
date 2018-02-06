95abde645550c7bb1e93a54326c1936f9224fbe1
Silvio <silvio.traversaro@iit.it>
2018-02-06 18:10:40 +0100

    Update code to iDynTree + remotecontrolboardremapper device

--- Module.cpp
+++ Module.cpp
@@ -1,8 +1,52 @@
 #include <Module.h>
-#include <yarpWholeBodyInterface/yarpWholeBodyInterface.h>
+
+#include <cmath>
+
 #include <yarp/os/ResourceFinder.h>
 #include <yarp/os/Property.h>
 #include <yarp/sig/Vector.h>
+
+// iDynTree headers
+#include <iDynTree/Core/VectorDynSize.h>
+#include <iDynTree/Model/FreeFloatingState.h>
+#include <iDynTree/Model/Model.h>
+#include <iDynTree/ModelIO/ModelLoader.h>
+#include <iDynTree/yarp/YARPConversions.h>
+
+void addVectorOfStringToProperty(yarp::os::Property& prop, std::string key, std::vector<std::string> & list)
+{
+    prop.addGroup(key);
+    yarp::os::Bottle & bot = prop.findGroup(key).addList();
+    for(size_t i=0; i < list.size(); i++)
+    {
+        bot.addString(list[i].c_str());
+    }
+    return;
+}
+
+void convertDegToRad(const yarp::sig::Vector& vecDeg, yarp::sig::Vector& vecRad)
+{
+    if (vecDeg.size() != vecRad.size()) {
+        yError() << "convertDegToRad: wrong vector size";
+        return;
+    }
+    
+    for (size_t i=0; i < vecDeg.size(); i++) {
+        vecRad[i] = (M_PI/180.0)*vecDeg[i];
+    }
+}
+
+void convertRadToDeg(const yarp::sig::Vector& vecRad, yarp::sig::Vector& vecDeg)
+{
+    if (vecDeg.size() != vecRad.size()) {
+        yError() << "convertDegToRad: wrong vector size";
+        return;
+    }
+    
+    for (size_t i=0; i < vecRad.size(); i++) {
+        vecDeg[i] = (180.0/M_PI)*vecRad[i];
+    }
+}
 
 double Module::getPeriod () { return 0.01; }
 
@@ -16,61 +60,129 @@ bool Module::updateModule ()
 bool Module::configure (yarp::os::ResourceFinder &rf)
 {
     using namespace yarp::os;
+    using namespace yarp::sig;
+    
+    ///////////////////////////////////////////////////////
+    //// Open the remotecontrolboardremapper YARP device 
+    ///////////////////////////////////////////////////////
+    
+    Property options;
+    options.put("device","remotecontrolboardremapper");
+    
+    
+    // Note: this joint list are tipically loaded from configuration 
+    // file and they are not harcoded in the code. However, to reduce 
+    // the complexity of the example we are hardcoding the lists.
+    // Torso Joints 
+    std::vector<std::string> axesList;
+    axesList.push_back("torso_pitch");
+    axesList.push_back("torso_roll");
+    axesList.push_back("torso_yaw");
+    
+    // Left arm
+    axesList.push_back("l_shoulder_pitch");
+    axesList.push_back("l_shoulder_roll");
+    axesList.push_back("l_shoulder_yaw");
+    axesList.push_back("l_elbow"); 
+    
+    // Right arm
+    axesList.push_back("r_shoulder_pitch");
+    axesList.push_back("r_shoulder_roll");
+    axesList.push_back("r_shoulder_yaw");
+    axesList.push_back("r_elbow"); 
+    
+    // Left leg 
+    axesList.push_back("l_hip_pitch");
+    axesList.push_back("l_hip_roll");
+    axesList.push_back("l_hip_yaw");
+    axesList.push_back("l_knee");
+    axesList.push_back("l_ankle_pitch");
+    axesList.push_back("l_ankle_roll");
+    
+    // Right leg 
+    axesList.push_back("r_hip_pitch");
+    axesList.push_back("r_hip_roll");
+    axesList.push_back("r_hip_yaw");
+    axesList.push_back("r_knee");
+    axesList.push_back("r_ankle_pitch");
+    axesList.push_back("r_ankle_roll");
+    
+    addVectorOfStringToProperty(options, "axesNames", axesList);
+    
+    Bottle remoteControlBoards;
+    Bottle & remoteControlBoardsList = remoteControlBoards.addList();
+    
+    // the iCub also expose a head part, that is not included because we are not controlling any head part 
+    remoteControlBoardsList.addString("/icub/torso");
+    remoteControlBoardsList.addString("/icub/left_arm");
+    remoteControlBoardsList.addString("/icub/right_arm");
+    remoteControlBoardsList.addString("/icub/left_leg");
+    remoteControlBoardsList.addString("/icub/right_leg");
+    
+    options.put("remoteControlBoards",remoteControlBoards.get(0));
+    options.put("localPortPrefix","/test");
+    Property & remoteControlBoardsOpts = options.addGroup("REMOTE_CONTROLBOARD_OPTIONS");
+    remoteControlBoardsOpts.put("writeStrict","on");
 
-    Property wbiProperties;
-    std::string wbiConfFile = rf.check("wbi_config_file", Value("yarpWholeBodyInterface.ini"), "Checking wbi configuration file").asString();
+    unsigned actuatedDOFs = axesList.size();
 
-    if (!wbiProperties.fromConfigFile(rf.findFile(wbiConfFile))) {
-        std::cout << "Not possible to load WBI properties from file.\n";
+    // Actually open the device 
+    bool ok = robotDevice.open(options);
+    if (!ok) {
+        std::cout << "Could not open remotecontrolboardremapper object.\n";
         return false;
     }
-    wbiProperties.fromString(rf.toString(), false);
 
-    //retrieve the joint list
-    std::string wbiList = rf.check("wbi_list", Value("ROBOT_TORQUE_CONTROL_JOINTS_WITHOUT_PRONOSUP"), "Looking for wbi list").asString();
+    // Try to obtain the interfaces 
+    ok=ok && robotDevice.view(ilim);
+    ok=ok && robotDevice.view(ienc);
+    ok=ok && robotDevice.view(imod);
+    ok=ok && robotDevice.view(itrq);
 
-    wbi::IDList iCubMainJoints;
-    if (!yarpWbi::loadIdListFromConfig(wbiList, wbiProperties, iCubMainJoints)) {
-        std::cout << "Cannot find joint list\n";
-        return false;
-    }
-
-    unsigned actuatedDOFs = iCubMainJoints.size();
-
-    //create an instance of wbi
-    m_robot = new yarpWbi::yarpWholeBodyInterface("computed_torque", wbiProperties);
-    if (!m_robot) {
-        std::cout << "Could not create wbi object.\n";
-        return false;
-    }
-
-    m_robot->addJoints(iCubMainJoints);
-    if (!m_robot->init()) {
-        std::cout << "Could not initialize wbi object.\n";
+    if (!ok) {
+        yError()<<"Unable to open interfaces";
         return false;
     }
     
-    //Creating and configuring a wbi instance.
-    //We use the yarp implementation.
-
-    //Load information about the joints and global properties for the wbi
-    //this file also contains references to the urdf model
+    ///////////////////////////////////////////////////////////////
+    //// Load the model in the iDynTree::KinDynComputations
+    ///////////////////////////////////////////////////////////////
+    
+    // We assume that the model.urdf can be found by the ResourceFinder:
+    // this means either that the file is in the current working directory, 
+    // or it is found using the ResourceFinder search hierarchy, documented in 
+    // http://www.yarp.it/yarp_data_dirs.html 
+    
+    std::string modelFullPath = rf.findFileByName("model.urdf");
+    
+    // We use the iDynTree::ModelLoader class to extract from the URDF file 
+    // a model containing only the joint we are interested in controlling, and 
+    // in the same order with which we configured the remotecontrolboardremapper
+    // device, to avoid complicated remapping between the vectors used in the YARP
+    // devices and the one used by the iDynTree model . 
+    iDynTree::ModelLoader mdlLoader;
+    ok = mdlLoader.loadReducedModelFromFile(modelFullPath, axesList);
+    
+    // Once we loaded the model, we pass it to the KinDynComputations class to 
+    // compute dynamics quantities such as the vector of gravity torques 
+    ok = ok && kinDynModel.loadRobotModel(mdlLoader.model());
+    
+    if (!ok) {
+        yError()<<"Unable to open model " << modelFullPath;
+        return false;
+    }
+    
+    const iDynTree::Model& model = kinDynModel.model();
     
     std::cout << "Number of DOFs: " << actuatedDOFs << "\n";
-
-    //Any further initialization
-    
     
     return true;
 }
 
 bool Module::close ()
 {
-
     //cleanup stuff
-    m_robot->close();
-    delete m_robot;
-    m_robot = 0;
+    robotDevice.close();
     return true;
 }
 
--- README.md
+++ README.md
@@ -6,8 +6,11 @@ Computed Torque with iCub
 To fully complete this assignment you should be familiar to the following components:
 
 - (Theory) Computed torque control
-- WholeBody Interface
+- The [`remotecontrolboardremapper`](http://www.yarp.it/classyarp_1_1dev_1_1RemoteControlBoardRemapper.html) YARP device 
 - YARP ports 
+- [`iDynTree::Model`](http://robotology.gitlab.io/docs/idyntree/master/classiDynTree_1_1Model.html), 
+[`iDynTree::ModelLoader`](http://robotology.gitlab.io/docs/idyntree/master/classiDynTree_1_1ModelLoader.html) 
+and [`iDynTree::KinDynComputations`](http://robotology.gitlab.io/docs/idyntree/master/classiDynTree_1_1KinDynComputations.html) iDynTree classes
 
 # Assignment
 You should develop a computed torque controller for the iCub humanoid robot.
@@ -19,6 +22,6 @@ The robot will be fixed in his root link (i.e. the pelvis), so you are not requi
 # Note
 
 We provide a skeleton of the YARP controller with the methods partially implemented. 
-In particular we implemented for you the initialization of the WholeBodyInterface object.
+In particular we implemented for you the initialization of the `remotecontrolboardremapper` YARP device and the `iDynTree::KinDynComputations` object.
 
 # [How to complete the assignment](https://github.com/vvv-school/vvv-school.github.io/blob/master/instructions/how-to-complete-assignments.md)
